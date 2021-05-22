5f0595c108c72233e21f11970cff65322feb6f3c
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-05-22 14:41:57 +0900

    start new frontend development

---
+++ parser/Parser.java
@@ -0,0 +1,25 @@
+public class Parser {
+	// TODO
+	static String parseHeader(String header) {	
+		return header;
+	}
+
+	static boolean checkSupportedLogic(String logic) {
+		return false;		
+	}
+
+	public static void main(String[] args) {
+		String logic;
+
+		if (args.length != 1) {
+			System.out.println("USAGE: java Parser <file.tslmt>");
+			System.exit(1);
+		}
+
+		logic = parseHeader(args[0]);
+		if (!checkSupportedLogic(logic)) {
+			System.out.printf("Unsupported logic: %s\n", logic);
+			System.exit(1);
+		}
+	}
+}
