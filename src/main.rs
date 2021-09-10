b4433a92a70498f48923dd6fcb263e2d331669e0
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-09-10 14:14:04 -0400

    start tsl_lia file

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ mod dto;
 mod json;
 mod lia;
 mod tsl;
+mod tsl_lia;
 mod specification;
 mod utils;
 mod sample;
 
 fn main() {
-    let _elevator = sample::elevator();
+    let elevator = sample::elevator();
+    let pred = &elevator.predicates[0];
+    println!("{}", pred.to_tsl_assumption());
 }
