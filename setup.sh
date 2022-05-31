3d495896b1a2ab3d6ed6280800ac3a288e25a9f0
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2022-05-31 10:57:57 -0700

    modify setup for oracling

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
-apt update
-apt install python3-pip
-python3 -m pip install --upgrade pip
-python3 -m pip install pandas colorama
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
+# sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+# apt update
+# apt install python3-pip
+# python3 -m pip install --upgrade pip
+# python3 -m pip install pandas colorama
+# curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+# source $HOME/.cargo/env
+rm -rf bin tsltools
 git clone https://github.com/wonhyukchoi/tsltools.git tsltools && cd tsltools && make
 mv cfm2code ../bin/
 mv tsl2tlsf ../bin/
